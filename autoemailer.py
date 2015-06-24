4c29bac0ea1c126b0bc124370a9f61cc7362c52e
Raj Ranjan <raj.ranjan61@yahoo.in>
2015-06-24 21:31:38 +0530

    Update autoemailer.py

--- autoemailer.py
+++ autoemailer.py
@@ -2,58 +2,57 @@
 from smtplib import SMTP
 from smtplib import SMTP_SSL
 from smtplib import SMTPException
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEImage import MIMEImage
 from email.mime.text import MIMEText
+from email import encoders
 import sys
 import MySQLdb
-
-def getemail():
-    alist=[]
-    # Open database connection Replace username, password with your username password and dbname with the name of database
-    db = MySQLdb.connect("localhost","username","password","dbname" )
-    # prepare a cursor object using cursor() method
-    cursor = db.cursor()
-    #replace emails with your table name containing a column 'email' holding the email addresses
-    sql = "SELECT * FROM emails"
-    try:
-       # Execute the SQL command
-       cursor.execute(sql)
-       # Fetch all the rows in a list of lists.
-       results = cursor.fetchall()
-       for row in results:
-           #replace 1 with your column number of email coloumn.(optional)
-          alist.append(row[1])
-      
-    except:
-       print "Error: unable to fecth data"
-   
-
-    # disconnect from server
-    db.close()
-    return alist
+from database import getemail
+from globalvar import EMAIL_SUBJECT
+from globalvar import EMAIL_SENDER
+from globalvar import TEXT_SUBTYPE
+from globalvar import YAHOO_SMTP
+from globalvar import YAHOO_SMTP_PORT
 
 
-#Global varialbes
-#EMAIL_SUBJECT = "Email from Python script"
-print 'Enter Subject'
-EMAIL_SUBJECT=str(raw_input())
-#replace abc@yahoo.com with your yahoo email id
-EMAIL_SENDER  =  'abc@yahoo.com'
-TEXT_SUBTYPE = "plain"
- 
-YAHOO_SMTP = "smtp.mail.yahoo.com"
-YAHOO_SMTP_PORT = 465
  
 def listToStr(lst):
     """This method makes comma separated list item string"""
     return ','.join(lst)
  
-def send_email(content, pswd, emailr):
-    """This method sends an email"""
-    msg = MIMEText(content, TEXT_SUBTYPE)
-    msg["Subject"] = EMAIL_SUBJECT
-    msg["From"] = EMAIL_SENDER
-    msg["To"] = listToStr(emailr)
-     
+def send_email(pswd, emailr):
+    #set attachement and body and attachement
+    COMMASPACE = ', '	
+    # Create the container (outer) email message.
+    msg = MIMEMultipart()
+    msg['Subject'] = EMAIL_SUBJECT
+    msg['From'] = EMAIL_SENDER
+    msg['To'] = COMMASPACE.join(emailr)
+    body = MIMEMultipart('alternative')
+    #attach body of email from email-body.txt
+    body.attach(MIMEText(file("email-body.txt").read(), 'plain'))   
+    msg.attach(body)
+    #replace filename with your filename and data to the address of attacments
+    attachments=[{'filename' : 'pdf.pdf', 'data' : file("pdf.pdf").read()},
+                {'filename' : 'email-body.txt', 'data' : file("email-body.txt").read()}]
+    for attachment in attachments:
+	    if attachment['filename'].find('doc') > 0:
+		    attachFile = MIMEBase('application', 'msword')     
+	    elif attachment['filename'].find('pdf') > 0:
+		    attachFile = MIMEBase('application', 'pdf')	
+	    else:
+		    attachFile = MIMEBase('application', 'octet-stream')    
+		
+	    attachFile.set_payload(attachment['data'])
+		
+	    encoders.encode_base64(attachFile)
+	    attachFile.add_header('Content-Disposition', 'attachment', filename=attachment['filename'])
+		
+	    msg.attach(attachFile);
+
+       
     try:
       #Yahoo allows SMTP connection over SSL. 
       smtpObj = SMTP_SSL(YAHOO_SMTP, YAHOO_SMTP_PORT)
@@ -61,20 +60,21 @@ def send_email(content, pswd, emailr):
       smtpObj.login(user=EMAIL_SENDER, password=pswd)
       smtpObj.sendmail(EMAIL_SENDER, emailr, msg.as_string())
       smtpObj.quit();
+      print 'Email succesfully sent.'
     except SMTPException as error:
       print "Error: unable to send email :  {err}".format(err=error)
  
-def main(content):
+def main():
     """This is a simple main() function which demonstrates sending of email using smtplib."""
     #replace yourapppassword with your own yahoo app password . Refer Readme for more information.
     pswd='yourapppassword'
     li=getemail()
-    send_email(content, pswd, li);
+    send_email(pswd, li);
  
 if __name__ == "__main__":
     """If this script is executed as stand alone then call main() function."""
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        print "Please provide message using main('message')"
+        print "call main() to send email"
         sys.exit(0)
